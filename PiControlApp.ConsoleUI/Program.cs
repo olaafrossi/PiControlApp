63c1d37278901e75aa34d3de4157518f3f17fa4a
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-06-13 19:08:49 -0400

    Add project files.

---
+++ PiControlApp.ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿// Created by Three Byte Intermedia, Inc.
+// project: PiControlApp
+// Created: 2021 06 12
+// by Olaaf Rossi
+// scp -r C:\Dev\PiControlApp\PiControlApp.ConsoleUI\bin\Release\net5.0\publish\* pi@pi01:Desktop/Deployment/
+
+using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace PiControlApp.ConsoleUI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var signalRConnection = new SignalRConnection();
+            signalRConnection.Start("Pi01", "Initial Connection");
+
+            Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
+            int pin = 18;
+
+            using var controller = new GpioController();
+            controller.OpenPin(pin, PinMode.Output);
+            bool ledOn = true;
+            while (true)
+            {
+                controller.Write(pin, ledOn ? PinValue.High : PinValue.Low);
+                signalRConnection.Start("Pi01", "Led Blink");
+                Thread.Sleep(1000);
+                ledOn = !ledOn;
+            }
+        }
+    }
+}
---
+++ PiControlApp.ConsoleUI/SignalRConnection.cs
@@ -0,0 +1,38 @@
+﻿// Created by Three Byte Intermedia, Inc.
+// project: PiControlApp
+// Created: 2021 06 13
+// by Olaaf Rossi
+
+using System;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace PiControlApp.ConsoleUI
+{
+    public class SignalRConnection
+    {
+        public async void Start(string user, string msg)
+        {
+            var url = "http://192.168.1.138:5000/ChatHub";
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl(url)
+                .WithAutomaticReconnect()
+                .Build();
+
+            // receive a message from the hub
+            connection.On<string, string>("ReceiveMessage", (user, message) => OnReceiveMessage(user, message));
+
+            var t = connection.StartAsync();
+
+            t.Wait();
+
+            // send a message to the hub
+            await connection.InvokeAsync("SendMessage", user, msg);
+        }
+
+        private void OnReceiveMessage(string user, string message)
+        {
+            Console.WriteLine($"{user}: {message}");
+        }
+    }
+}
