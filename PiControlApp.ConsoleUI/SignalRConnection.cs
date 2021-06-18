757cf9554f4af985a61867b0a99863ebcc2e68b8
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-06-18 02:07:39 -0400

    Adds BLE280 env sensor

--- PiControlApp.ConsoleUI/SignalRConnection.cs
+++ PiControlApp.ConsoleUI/SignalRConnection.cs
@@ -4,30 +4,50 @@
 // by Olaaf Rossi
 
 using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
 
 namespace PiControlApp.ConsoleUI
 {
     public class SignalRConnection
     {
-        public async void Start(string user, string msg)
-        {
-            var url = "http://192.168.1.138:5000/ChatHub";
+        private HubConnection _chatHub;
 
-            var connection = new HubConnectionBuilder()
+        public async void Start()
+        {
+            string url = "http://192.168.1.138:5000/ChatHub";
+
+            HubConnection connection = new HubConnectionBuilder()
                 .WithUrl(url)
                 .WithAutomaticReconnect()
                 .Build();
 
+            _chatHub = connection;
+
             // receive a message from the hub
             connection.On<string, string>("ReceiveMessage", (user, message) => OnReceiveMessage(user, message));
-
-            var t = connection.StartAsync();
+            connection.On<string, int>("IntReceive", (s, i) => OnIntReceive(s, i));
+            Task t = connection.StartAsync();
 
             t.Wait();
 
             // send a message to the hub
-            await connection.InvokeAsync("SendMessage", user, msg);
+            //await connection.InvokeAsync("SendMessage", user, msg);
+        }
+
+        private async void OnIntReceive(string s, int i)
+        {
+            Console.WriteLine($"{s}: {i}");
+        }
+
+        public async void SendMessage(string user, string msg)
+        {
+            await _chatHub.InvokeAsync("SendMessage", user, msg);
+        }
+
+        public async void SendInt(string user, int msg)
+        {
+            await _chatHub.InvokeAsync("SendMessage", user, msg);
         }
 
         private void OnReceiveMessage(string user, string message)
