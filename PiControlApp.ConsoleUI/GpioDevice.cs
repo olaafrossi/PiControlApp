01f8b0ec2c63f9f1fc8d8e5d1fdcab50e7feb721
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-06-19 15:14:43 -0400

    Adds Gpio Device to it's own class
    refs #1

---
+++ PiControlApp.ConsoleUI/GpioDevice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Gpio;
+
+namespace PiControlApp.ConsoleUI
+{
+    public class GpioDevice
+    {
+        private readonly GpioController _controller;
+        const int pin = 18;
+
+        public GpioDevice()
+        {
+            GpioController controller = new();
+            _controller = controller;
+            _controller.OpenPin(pin, PinMode.Output);
+        }
+
+        /// <summary>
+        /// Turns on/off the LED connected to Pin 18
+        /// </summary>
+        /// <param name="state"></param>
+        public void LedOn(bool state)
+        {
+            if (state is true)
+            {
+                _controller.Write(pin, PinValue.High);
+            }
+            else
+            {
+                _controller.Write(pin, PinValue.Low);
+            }
+        }
+    }
+}
