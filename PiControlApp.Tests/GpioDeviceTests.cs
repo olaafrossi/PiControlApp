5712c5b894b2cbb832907e7b99bf05b56db4c959
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-10-21 15:13:48 -0400

    adds testing project

---
+++ PiControlApp.Tests/GpioDeviceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using PiControlApp.ConsoleUI.Devices;
+using Xunit;
+using Moq;
+using Moq.Language.Flow;
+
+namespace PiControlApp.Tests
+{
+
+    public class GpioDeviceTests 
+    {
+        [Fact]
+        public void LedState_ShouldBeNegative()
+        {
+            Mock<IGpioDevice> device = new();
+
+            // Arrange
+            bool expected = false;
+
+            //Act
+            ISetup<IGpioDevice> actual = device.Setup(mock => mock.LedState(true));
+            bool act = actual.Equals(true);
+
+            // Assert
+            Assert.Equal(expected, act);
+
+        }
+    }
+}
